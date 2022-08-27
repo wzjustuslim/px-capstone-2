26df128c0ac65da0c87102cc3bb33cb9f67e3091
wzjustuslim <weizhengjustus.lim@gmail.com>
2022-08-27 22:01:08 +0800

    landing page phase two

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,13 +5,17 @@ import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import LoginIcon from '@mui/icons-material/Login';
+import LocalMallIcon from '@mui/icons-material/LocalMall';
+import RedeemIcon from '@mui/icons-material/Redeem';
 
 const LandingPage = () => {
 
   return (
     <main>
       <section>
-        <Container maxWidth="xl" sx={{ mt: "68.5px" }}>
+        <Container maxWidth="xl" sx={{ pt: "68.5px" }}>
         <Grid container spacing={0}>
           <Grid xs={12} md={6}>
             <Box sx={{ 
@@ -19,7 +23,7 @@ const LandingPage = () => {
               flexDirection: "column",
               justifyContent: "center",
               height: "100%",
-              mx: 8
+              px: 8
             }}>
               {/* Fontsize and alignment not response at xs size */}
               <Typography
@@ -62,38 +66,113 @@ const LandingPage = () => {
           </Grid>
           <Grid xs={12} md={6}>
             {/* Image size is not response at xs */}
-            <Paper className="hero-img" sx={{ m: 8, borderRadius: 4 }}>
-            </Paper>
+            <Box sx={{ py: 8, pl: 16 }}>
+              <Paper className="hero-img" sx={{ borderRadius: 4 }}></Paper>
+            </Box>
           </Grid>
         </Grid>
         </Container>
       </section>
       <section>
-        <Container maxWidth="xl">
-          <Grid container spacing={14} sx={{ mx: 0 }}>
-            <Grid xs={12} md={6} lg={3}>
-              <Typography>
-                Sed varius venenatis turpis varius tempor. Phasellus faucibus a erat eu mattis. Cras tincidunt accumsan scelerisque. In semper lorem sit amet eros accumsan mattis. Sed consequat quis sapien in placerat.
-              </Typography>
+        <Box className="instruction-section">
+          <Container maxWidth="xl" sx={{ pb: 16 }}>
+            <Grid container spacing={0}>
+              <Grid xs={12}>
+                <Box sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  py: 6
+                }}>
+                  <Typography
+                    variant="h5"
+                    sx={{
+                      fontWeight: 700
+                    }}
+                  >
+                    How to get started
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid xs={12} md={6} lg={3}>
+                <Box sx={{
+                  px: 6
+                }}>
+                  <LoginIcon fontSize="large" color="primary" sx={{ mb: 1 }} />
+                  <Typography
+                    variant="subtitle1"
+                    gutterBottom="true"
+                    sx={{
+                      fontWeight: 700
+                    }}
+                  >
+                    Sign up
+                  </Typography>
+                  <Typography>
+                    Sed varius venenatis turpis varius tempor. Phasellus faucibus a erat eu mattis. Cras tincidunt accumsan scelerisque. In semper lorem sit amet eros accumsan mattis.
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid xs={12} md={6} lg={3}>
+                <Box sx={{
+                  px: 6
+                }}>
+                  <AccountBalanceWalletIcon fontSize="large" color="primary" sx={{ mb: 1 }} />
+                  <Typography
+                    variant="subtitle1"
+                    gutterBottom="true"
+                    sx={{
+                      fontWeight: 700
+                    }}
+                  >
+                    Top up your wallet
+                  </Typography>
+                  <Typography>
+                    Sed varius venenatis turpis varius tempor. Phasellus faucibus a erat eu mattis. Cras tincidunt accumsan scelerisque. In semper lorem sit amet eros accumsan mattis.
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid xs={12} md={6} lg={3}>
+                <Box sx={{
+                  px: 6
+                }}>
+                  <LocalMallIcon fontSize="large" color="primary" sx={{ mb: 1 }} />
+                  <Typography
+                    variant="subtitle1"
+                    gutterBottom="true"
+                    sx={{
+                      fontWeight: 700
+                    }}
+                  >
+                    Buy your items
+                  </Typography>
+                  <Typography>
+                    Sed varius venenatis turpis varius tempor. Phasellus faucibus a erat eu mattis. Cras tincidunt accumsan scelerisque. In semper lorem sit amet eros accumsan mattis.
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid xs={12} md={6} lg={3}>
+                <Box sx={{
+                  px: 6
+                }}>
+                  <RedeemIcon fontSize="large" color="primary" sx={{ mb: 1 }} />
+                  <Typography
+                    variant="subtitle1"
+                    gutterBottom="true"
+                    sx={{
+                      fontWeight: 700
+                    }}
+                  >
+                    Redeem items in-game
+                  </Typography>
+                  <Typography>
+                    Sed varius venenatis turpis varius tempor. Phasellus faucibus a erat eu mattis. Cras tincidunt accumsan scelerisque. In semper lorem sit amet eros accumsan mattis.
+                  </Typography>
+                </Box>
+              </Grid>
             </Grid>
-            <Grid xs={12} md={6} lg={3}>
-              <Typography>
-                Sed varius venenatis turpis varius tempor. Phasellus faucibus a erat eu mattis. Cras tincidunt accumsan scelerisque. In semper lorem sit amet eros accumsan mattis. Sed consequat quis sapien in placerat.
-              </Typography>
-            </Grid>
-            <Grid xs={12} md={6} lg={3}>
-              <Typography>
-                Sed varius venenatis turpis varius tempor. Phasellus faucibus a erat eu mattis. Cras tincidunt accumsan scelerisque. In semper lorem sit amet eros accumsan mattis. Sed consequat quis sapien in placerat.
-              </Typography>
-            </Grid>
-            <Grid xs={12} md={6} lg={3}>
-              <Typography>
-                Sed varius venenatis turpis varius tempor. Phasellus faucibus a erat eu mattis. Cras tincidunt accumsan scelerisque. In semper lorem sit amet eros accumsan mattis. Sed consequat quis sapien in placerat.
-              </Typography>
-            </Grid>
-          </Grid>
 
-        </Container>
+          </Container>
+        </Box>
       </section>
     </main>
   )
