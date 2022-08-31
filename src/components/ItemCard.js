f619721a73307aabc7d746f4be517493fc5ba533
daily-dev-jake <jlele92@outlook.com>
2022-08-31 09:25:44 +0800

    include qty textfield

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,12 +3,15 @@ import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import TextField from '@mui/material/TextField';
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import { Box, CardActionArea } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
 import Img from "../static/images/pokeball-card.jpg";
 import "./ItemCard.css";
+
+// Button will use router path to add to cart sidebar
 
 export default function ItemCard({
   id,
@@ -27,6 +30,7 @@ export default function ItemCard({
           height='160'
           image={itemImage}
           alt={itemName}
+          sx = {{scale:'1'}}
         />
         <CardContent>
           <Typography
@@ -46,16 +50,27 @@ export default function ItemCard({
             {itemPrice}
           </Typography>
           <Typography variant='h6'>{itemCategory}</Typography>
+          <Box component='span'>
+          <TextField
+          id="outlined-number"
+          label="Qty"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          sx={{width:'7rem'}}
+        />
           <Button
             component={RouterLink}
             to={`#`}
             variant='contained'
             size='large'
             sx={{
-              px: 6,
+              px: 2,
               py: 2,
               borderRadius: 3,
             }}>Buy</Button>
+            </Box>
         </CardContent>
       </CardActionArea>
     </Card>
