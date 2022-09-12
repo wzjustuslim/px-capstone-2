0364cb5d60ce13f5c5e251cf2c2c73d75954a84c
daily-dev-jake <jlele92@outlook.com>
2022-09-12 23:33:09 +0800

    Prep to display qty on TempDrawer

--- src/components/ExploreCard.js
+++ src/components/ExploreCard.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useContext, useRef, useState } from "react";
 import Grid from "@mui/material/Unstable_Grid2";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
@@ -6,14 +6,40 @@ import CardActionArea from "@mui/material/CardActionArea";
 import CardMedia from "@mui/material/CardMedia";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
 
+import toProper from "../helper/toProper";
+import CartContext from "../contexts/cart-context";
 export default function ExploreCard({ item }) {
-  const { itemImage, itemName, itemPrice } = item;
+  const { _id, itemImage, itemName, itemPrice } = item;
+  // const qtyItemRef = useRef();
+  const cartCtx = useContext(CartContext);
+  // const [enteredAmountIsValid, setEnteredAmountIsValid] = useState(true);
 
-  const handleItemClick = () => {
-    console.log(item);
+  const addItemToCartHandler = (qty) => {
+    cartCtx.addItem({
+      id: _id,
+      name: toProper(itemName),
+      image: itemImage,
+      qty: qty,
+      price: itemPrice,
+    });
+  };
+
+  const addToCartHandler = (e) => {
+    e.preventDefault();
+    // console.log(qtyItemRef);
+    // console.log(qtyItemRef.current.value);
+    // const enteredQty = qtyItemRef.current.value;
+    // const enteredQtyNum = +enteredQty;
+
+    // if (enteredQty.trim().length === 0) {
+    //   setEnteredAmountIsValid(false);
+    //   return;
+    // }
+    // setEnteredAmountIsValid(true);
+    addItemToCartHandler(1);
   };
 
   return (
@@ -24,7 +50,7 @@ export default function ExploreCard({ item }) {
         sx={{
           borderRadius: "12px",
         }}>
-        <CardActionArea onClick={handleItemClick}>
+        <CardActionArea onClick={addToCartHandler}>
           <CardContent sx={{ pb: 1 }}>
             <Box
               sx={{
@@ -69,23 +95,43 @@ export default function ExploreCard({ item }) {
               </Box>
             </Box>
           </CardContent>
-          <CardActions
-            className='explore-card-actions'
-            sx={{
-              bgcolor: "primary.main",
-              display: "flex",
-              justifyContent: "center",
-            }}>
-            <Typography
-              align='center'
-              variant='subtitle2'
-              color='white'
+          {false ? (
+            <CardActions
+              className='explore-card-actions'
               sx={{
-                fontWeight: "700",
+                bgcolor: "primary.main",
+                display: "flex",
+                justifyContent: "center",
               }}>
-              Buy now
-            </Typography>
-          </CardActions>
+              <Typography
+                align='center'
+                variant='subtitle2'
+                color='white'
+                sx={{
+                  fontWeight: "700",
+                }}>
+                Add to Cart
+              </Typography>
+            </CardActions>
+          ) : (
+            <CardActions
+              className='explore-card-actions'
+              sx={{
+                bgcolor: "red",
+                display: "flex",
+                justifyContent: "center",
+              }}>
+              <Typography
+                align='center'
+                variant='subtitle2'
+                color='white'
+                sx={{
+                  fontWeight: "700",
+                }}>
+                Remove From Cart
+              </Typography>
+            </CardActions>
+          )}
         </CardActionArea>
       </Card>
     </Grid>
